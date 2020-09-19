072f031b4280d8cf1b512b4aaa362b1a7ce539c4
raj-vora <raj.jv@somaiya.edu>
2020-09-19 11:20:24 +0530

    first push 19/9

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,96 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/raj.jpg'),
+                ),
+                Text(
+                  'Raj Vora',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  'Web developer',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal[100],
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0, 
+                    horizontal: 25.0
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal
+                    ),
+                    title: Text(
+                      '9619073040',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      )
+                    ),
+                  )
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0, 
+                    horizontal: 25.0
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'rajvora1998@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      )
+                    ),
+                  )
+                ),
+              ],
+            ),
+          ),
+        )
       ),
-    ),
-  );
+    );
+  }
 }
